ef45cd267d93ae30a313ff48edcd5da4dbe62d95
Ranjan Shrestha <ranjan@sharingapples.com>
2017-01-22 17:11:44 +0545

    Fixed the order in which the configuration checking is done

--- src/ReduxStateChangeListener.js
+++ src/ReduxStateChangeListener.js
@@ -13,36 +13,35 @@ class ReduxStateChangeListener {
   start() {
     // Get the original state (to check the difference with)
     this._state = this._store.getState();
-    this._mappedValues = this._mappers(o => o.mapper(this._state));
+    this._mappedValues = this._mappers.map(event => event.mapper(this._state));
 
     // Subscribe to the redux store
-    this._unsubscribe = this._store.subscribe(this._callback);
-  }
+    this._unsubscribe = this._store.subscribe(() => {
+      const nextState = this._store.getState();
+      this._mappedValues = this._mappers.map((event, idx) => {
+        const newValue = event.mapper(nextState);
 
-  _callback = () => {
-    const nextState = this._store.getState();
-    this._mappedValues = this._mappers.map((event, idx) => {
-      const newValue = event.mapper(nextState);
+        // handle if the mapper provided a complex logic
+        if (event.fireOnTrue && newValue) {
+          event.callback(nextState, this._state);
+          return newValue;
+        }
 
-      // See if ignore null has been set
-      if (event.ignoreNull && newValue === null) {
+        // See if ignore null has been set
+        if (event.ignoreNull && newValue === null) {
+          return newValue;
+        }
+
+
+        // The normal use case checking if the values for the given
+        // state part has changed
+        const oldValue = this._mappedValues[idx];
+        if (newValue !== oldValue) {
+          event.callback(newValue, oldValue);
+        }
+
         return newValue;
-      }
-
-      // handle if the mapper provided a complex logic
-      if (event.fireOnTrue && newValue) {
-        event.callback(nextState, this._state);
-        return newValue;
-      }
-
-      // The normal use case checking if the values for the given
-      // state part has changed
-      const oldValue = this._mappedValues[idx];
-      if (newValue !== oldValue) {
-        event.callback(newValue, oldValue);
-      }
-
-      return newValue;
+      });
     });
   }
 
